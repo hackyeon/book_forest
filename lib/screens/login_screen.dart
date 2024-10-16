3aeeeb95bec6cd2c3b58f705049ab57bffd49280
hack <skek933@gmail.com>
2024-10-16 22:26:36 +0900

    login 로딩뷰

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -15,13 +15,22 @@ class _LoginScreenState extends State<LoginScreen> {
   final _codeController = TextEditingController();
   bool _isCodeSent = false;
   bool _isEmailEditable = true;
+  bool _isLoading = false; // 로딩 상태를 관리하는 변수
 
   _sendVerificationCode() async {
+    setState(() {
+      _isLoading = true; // 로딩 시작
+    });
+
     // 여기에 인증 코드 발송 API를 추가하세요.
     var json = {'email': _emailController.text};
     var response = await api.request(Url.LOGIN_MAIL, json);
 
-    if(response['result'] != 0) {
+    setState(() {
+      _isLoading = false; // 로딩 종료
+    });
+
+    if (response['result'] != 0) {
       toast(response['message']);
       return;
     }
@@ -35,12 +44,20 @@ class _LoginScreenState extends State<LoginScreen> {
   _verifyCode() async {
     // 여기에 인증 코드 확인 API를 추가하세요.
     if (_codeController.text.isNotEmpty) {
+      setState(() {
+        _isLoading = true; // 로딩 시작
+      });
+
       // 인증이 성공하면 다음 화면으로 이동.
       var email = _emailController.text;
       var json = {'email': email, 'code': _codeController.text};
       var response = await api.request(Url.LOGIN, json);
 
-      if(response['result'] != 0) {
+      setState(() {
+        _isLoading = false; // 로딩 종료
+      });
+
+      if (response['result'] != 0) {
         toast(response['message']);
         return;
       }
@@ -57,10 +74,10 @@ class _LoginScreenState extends State<LoginScreen> {
   // 메일 다시 적기 기능
   _resetEmailInput() {
     setState(() {
-      _isCodeSent = false;  // 인증 코드 입력 필드 숨김
-      _isEmailEditable = true;  // 이메일 다시 수정 가능하게 변경
-      _emailController.clear();  // 이메일 입력 필드 초기화
-      _codeController.clear();   // 인증 코드 필드 초기화
+      _isCodeSent = false; // 인증 코드 입력 필드 숨김
+      _isEmailEditable = true; // 이메일 다시 수정 가능하게 변경
+      _emailController.clear(); // 이메일 입력 필드 초기화
+      _codeController.clear(); // 인증 코드 필드 초기화
     });
   }
 
@@ -76,46 +93,49 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // 이메일 입력 또는 텍스트로 변환된 이메일 표시
-            _isEmailEditable
-                ? TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: '이메일 입력',
-              ),
-            )
-                : Text(
-              _emailController.text,
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 20),
+      body: _isLoading // 로딩 중일 때 로딩 화면 표시
+          ? Center(child: CircularProgressIndicator()) // 로딩 중일 때 로딩 표시
+          : Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // 이메일 입력 또는 텍스트로 변환된 이메일 표시
+                  _isEmailEditable
+                      ? TextField(
+                          controller: _emailController,
+                          decoration: InputDecoration(
+                            labelText: '이메일 입력',
+                          ),
+                        )
+                      : Text(
+                          _emailController.text,
+                          style: TextStyle(fontSize: 18),
+                        ),
+                  SizedBox(height: 20),
 
-            // 인증 코드 입력 필드 (메일이 발송된 후에만 보임)
-            if (_isCodeSent)
-              TextField(
-                controller: _codeController,
-                decoration: InputDecoration(
-                  labelText: '인증번호 입력',
-                ),
+                  // 인증 코드 입력 필드 (메일이 발송된 후에만 보임)
+                  if (_isCodeSent)
+                    TextField(
+                      controller: _codeController,
+                      decoration: InputDecoration(
+                        labelText: '인증번호 입력',
+                      ),
+                    ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed:
+                        _isCodeSent ? _verifyCode : _sendVerificationCode,
+                    child: Text(_isCodeSent ? '인증번호 확인' : '인증번호 발송'),
+                  ),
+                  if (_isCodeSent)
+                    TextButton(
+                      onPressed: _resetEmailInput,
+                      child: Text('메일 다시 적기'),
+                    ),
+                ],
               ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _isCodeSent ? _verifyCode : _sendVerificationCode,
-              child: Text(_isCodeSent ? '인증번호 확인' : '인증번호 발송'),
             ),
-            if (_isCodeSent)
-              TextButton(
-                onPressed: _resetEmailInput,
-                child: Text('메일 다시 적기'),
-              ),
-          ],
-        ),
-      ),
     );
   }
 }
